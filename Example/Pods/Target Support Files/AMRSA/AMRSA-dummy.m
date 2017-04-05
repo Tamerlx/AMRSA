0f466760a54e943265887babff17e41b780fe95d
liuxu5 <liuxu5@vipkid.com.cn>
2017-04-05 15:26:46 +0800

    Create lib

---
+++ Example/Pods/Target Support Files/AMRSA/AMRSA-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AMRSA : NSObject
+@end
+@implementation PodsDummy_AMRSA
+@end
---
+++ Example/Pods/Target Support Files/Pods-AMRSA_Tests/Pods-AMRSA_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AMRSA_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_AMRSA_Tests
+@end
